4a6f67790861bdc6f65db0df4730e8135cd73f72
Oleg Kapitonov <kapitonov1987@gmail.com>
2020-01-09 20:14:47 +0300

    bump to version 1.1

--- LV2/kpp_fuzz/kpp_fuzz_ui.src/kpp_fuzz_gui.c
+++ LV2/kpp_fuzz/kpp_fuzz_ui.src/kpp_fuzz_gui.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Oleg Kapitonov
+ * Copyright (C) 2018-2020 Oleg Kapitonov
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
